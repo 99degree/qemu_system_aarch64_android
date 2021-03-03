c3d7088b381f39cb7602778c7349a73e540bb5cb
99degree <49397123+99degree@users.noreply.github.com>
2021-03-04 03:13:35 +0800

    Update shell.sh
    Add telnet support

--- shell.sh
+++ shell.sh
@@ -3,4 +3,5 @@
 /sbin/busybox ifconfig eth0 10.0.2.15 netmask 255.255.255.0 up
 /sbin/busybox route add default gw 10.0.2.2 eth0
 /sbin/busybox ulimit -S -c unlimited
+/sbin/busybox telnetd -b 10.0.2.15:22 -l /sbin/sh
 
